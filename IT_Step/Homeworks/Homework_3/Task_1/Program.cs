6262d97b1a111b17f7a4f0038b080bcc38d2c807
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-01-06 11:39:34 +0200

    Added Homework_3

---
+++ IT_Step/Homeworks/Homework_3/Task_1/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+ ============================================================================
+ Name        : Homework_3-Task_1
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Create a "Website" class. The following must be stored in the class fields: site name, site path, site description, site ip address.
+Implement class methods for data input, data output. Implement access to individual fields through class methods.
+ ============================================================================
+ */
+
+namespace Task_1
+{
+    class MySite
+    {
+        public string m_sName_ { get; set; }
+        public string m_sWebAddress_ { get; set; }
+        public string m_sIpAddress_ { get; set; }
+        public string m_sDescription_ { get; set; }
+
+        public MySite()
+        {
+            m_sName_ = "No name";
+            m_sWebAddress_ = "No web-address";
+            m_sIpAddress_ = "No ip-address";
+            m_sDescription_ = "No description";
+        }
+
+        public MySite(string _sName, string _sWebAddress, string _sIpAddress, string _sDescription)
+        {
+            m_sName_ = _sName;
+            m_sWebAddress_ = _sWebAddress;
+            m_sIpAddress_ = _sIpAddress;
+            m_sDescription_ = _sDescription;
+        }
+    }
+
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            MySite site_1 = new MySite();
+
+            Console.WriteLine("Сайт №1 (начальный) :");
+            Console.WriteLine(site_1.m_sName_);
+            Console.WriteLine(site_1.m_sWebAddress_);
+            Console.WriteLine(site_1.m_sIpAddress_);
+            Console.WriteLine(site_1.m_sDescription_);
+            Console.WriteLine();
+
+            site_1.m_sName_ = "Новое имя";
+            site_1.m_sWebAddress_ = "Новый веб-адрес";
+            site_1.m_sIpAddress_ = "Новый ip-адрес";
+            site_1.m_sDescription_ = "Новое описание";
+
+            Console.WriteLine("Сайт №1 (обновленный) :");
+            Console.WriteLine(site_1.m_sName_);
+            Console.WriteLine(site_1.m_sWebAddress_);
+            Console.WriteLine(site_1.m_sIpAddress_);
+            Console.WriteLine(site_1.m_sDescription_);
+            Console.WriteLine();
+
+            MySite site_2 = new MySite("Мой сайт", "https://mywebsite.com", "192.168.0.1", "Очень интересный сайт");
+
+            Console.WriteLine("Сайт №2 (начальный) :");
+            Console.WriteLine(site_2.m_sName_);
+            Console.WriteLine(site_2.m_sWebAddress_);
+            Console.WriteLine(site_2.m_sIpAddress_);
+            Console.WriteLine(site_2.m_sDescription_);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IT_Step/Homeworks/Homework_3/Task_2/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+ ============================================================================
+ Name        : Homework_3-Task_2
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Create the "Shop" class. The following must be stored in the class fields: store name, address, store profile description,
+contact phone number, email. Implement class methods for data input, data output. Implement access to individual fields via
+  class methods.
+ ============================================================================
+ */
+
+namespace Task_2
+{
+    class MyShop
+    {
+        public string m_sName_ { get; set; }
+        public string m_sAddress_ { get; set; }
+        public string m_sDescription_ { get; set; }
+        public string m_sPhoneNumber_ { get; set; }
+        public string m_sEmail_ { get; set; }
+
+        public MyShop()
+        {
+            m_sName_ = "No name";
+            m_sAddress_ = "No address";
+            m_sDescription_ = "No description";
+            m_sPhoneNumber_ = "000-000-000";
+            m_sEmail_ = "No email";
+        }
+
+        public MyShop(string _sName, string _sAddress, string _sDescription, string _sPhoneNumber, string _sEmail)
+        {
+            m_sName_ = _sName;
+            m_sAddress_ = _sAddress;
+            m_sDescription_ = _sDescription;
+            m_sPhoneNumber_ = _sPhoneNumber;
+            m_sEmail_ = _sEmail;
+        }
+    }
+
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            MyShop shop_1 = new MyShop();
+
+            Console.WriteLine("Магазин №1 (начальный) :");
+            Console.WriteLine(shop_1.m_sName_);
+            Console.WriteLine(shop_1.m_sAddress_);
+            Console.WriteLine(shop_1.m_sDescription_);
+            Console.WriteLine(shop_1.m_sPhoneNumber_);
+            Console.WriteLine(shop_1.m_sEmail_);
+            Console.WriteLine();
+
+            shop_1.m_sName_ = "Новое имя";
+            shop_1.m_sAddress_ = "Новый адрес";
+            shop_1.m_sDescription_ = "Новое описание";
+            shop_1.m_sPhoneNumber_ = "123-456-789";
+            shop_1.m_sEmail_ = "Новый email";
+
+            Console.WriteLine("Магазин №1 (обновленный) :");
+            Console.WriteLine(shop_1.m_sName_);
+            Console.WriteLine(shop_1.m_sAddress_);
+            Console.WriteLine(shop_1.m_sDescription_);
+            Console.WriteLine(shop_1.m_sPhoneNumber_);
+            Console.WriteLine(shop_1.m_sEmail_);
+            Console.WriteLine();
+
+            MyShop shop_2 = new MyShop("Мой магазин", "Адрес магазина", "Описание магазина", "987-654-321", "Email магазина");
+
+            Console.WriteLine("Магазин №2 (начальный) :");
+            Console.WriteLine(shop_2.m_sName_);
+            Console.WriteLine(shop_2.m_sAddress_);
+            Console.WriteLine(shop_2.m_sDescription_);
+            Console.WriteLine(shop_2.m_sPhoneNumber_);
+            Console.WriteLine(shop_2.m_sEmail_);
+
+            Console.ReadLine();
+        }
+    }
+}
