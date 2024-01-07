b1413024b7fc1675c676083e051f8b42db8bd625
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-01-07 10:41:22 +0200

    Added Homework_4

---
+++ IT_Step/Homeworks/Homework_4/Task_1/Program.cs
@@ -0,0 +1,217 @@
+﻿/*
+ ============================================================================
+ Name        : Homework_4-Task_1
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Program the Money class (class object operates with one currency) to work with money.
+  The classroom should have: a field for storing a whole part of money (dollars, euros, hryvnias, etc.) and a field
+  for storing pennies (cents, eurocents, pennies, etc.). Implement methods of displaying the amount on the screen, tasks
+  values of parts. Based on the Money class, create a Product class to work with a product or commodity. Implement a method that allows
+  reduce the price by a given number. For each of the classes, implement the necessary methods and fields.
+ ============================================================================
+ */
+
+namespace Task_1
+{
+    class Money
+    {
+        private int m_iWholePart_;
+        private int m_iFractionPart_;
+
+        public Money() : this(0, 0) { }
+
+        public Money(int _WholePart, int _FractionPart)
+        {
+            if (_WholePart < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (_FractionPart < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            m_iWholePart_ += _WholePart;
+
+            if (_FractionPart <= 99)
+            {
+                m_iFractionPart_ = _FractionPart;
+            }
+            else
+            {
+                // Если кол-во разменных монет > 99, то перевести его в целую часть
+                int iTemp = _FractionPart / 100;
+                m_iWholePart_ += iTemp;
+                m_iFractionPart_ += _FractionPart - iTemp * 100;
+            }
+
+        }
+
+        public int WholePart
+        {
+            get { return m_iWholePart_; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                m_iWholePart_ = value;
+            }
+        }
+
+        public int FractionPart
+        {
+            get { return m_iFractionPart_; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                // Добавить кол-во разменных монет к имеющемуся
+                m_iFractionPart_ = value;
+
+                // Если кол-во разменных монет стало > 99, то перевести его в целую часть
+                if (m_iFractionPart_ > 99)
+                {
+                    int iTemp = m_iFractionPart_ / 100;
+                    m_iWholePart_ += iTemp;
+                    m_iFractionPart_ += value - iTemp * 100;
+
+                }
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(WholePart + "." + FractionPart);
+
+            return;
+        }
+    }
+
+    class Product : Money
+    {
+        private string m_sName_;
+
+        public Product()
+           : base(0, 0)
+        {
+            m_sName_ = "Название не указано";
+        }
+
+        public Product(string _Name, int _WholePart, int _FractionPart)
+            : base(_WholePart, _FractionPart)
+        {
+            m_sName_ = _Name;
+        }
+
+        public void SetStartPrice()
+        {
+            Console.WriteLine("Начальная цена продукта :");
+
+            Console.WriteLine("Целые единицы :");
+            int WholePart = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Разменные единицы :");
+            int FractionPart = Convert.ToInt32(Console.ReadLine());
+
+            SetStartPrice(WholePart, FractionPart);
+
+            return;
+        }
+
+        public void SetStartPrice(int _iWholePart, int _iFractionPart)
+        {
+            base.WholePart = _iWholePart;
+            base.FractionPart = _iFractionPart;
+        }
+
+        public void IncreasePrice()
+        {
+            Console.WriteLine("На сколько увеличить цену ?");
+
+            Console.WriteLine("Целые единицы :");
+            int WholePart = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Разменные единицы :");
+            int FractionPart = Convert.ToInt32(Console.ReadLine());
+
+            IncreasePrice(WholePart, FractionPart);
+        }
+
+        public void IncreasePrice(int _iWholePart, int _iFractionPart)
+        {
+            base.WholePart += _iWholePart;
+            base.FractionPart += _iFractionPart;
+        }
+
+        public void DecreasePrice()
+        {
+            Console.WriteLine("На сколько уменьшить цену ?");
+
+            Console.WriteLine("Целые единицы :");
+            int WholePart = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Разменные единицы :");
+            int FractionPart = Convert.ToInt32(Console.ReadLine());
+
+            DecreasePrice(WholePart, FractionPart);
+        }
+
+        public void DecreasePrice(int _iWholePart, int _iFractionPart)
+        {
+            base.WholePart -= _iWholePart;
+            base.FractionPart -= _iFractionPart;
+        }
+
+        public string GetCurrentPrice()
+        {
+            return Convert.ToString(base.WholePart) + "." + Convert.ToString(base.FractionPart);
+        }
+
+        public string GetName()
+        {
+            return m_sName_;
+        }
+    }
+
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Product myProduct_1 = new Product("Новый продукт", 10, 200);
+            Console.WriteLine("Текущая цена продукта " + myProduct_1.GetName() + " :" + myProduct_1.GetCurrentPrice());
+
+            myProduct_1.SetStartPrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_1.GetCurrentPrice());
+
+            myProduct_1.IncreasePrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_1.GetCurrentPrice());
+
+            myProduct_1.DecreasePrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_1.GetCurrentPrice());
+
+            Console.ReadLine();
+
+            Product myProduct_2 = new Product();
+            Console.WriteLine("Текущая цена продукта " + myProduct_2.GetName() + " :" + myProduct_2.GetCurrentPrice());
+
+            myProduct_2.SetStartPrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_2.GetCurrentPrice());
+
+            myProduct_2.IncreasePrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_2.GetCurrentPrice());
+
+            myProduct_2.DecreasePrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_2.GetCurrentPrice());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IT_Step/Homeworks/Homework_4/Task_2/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+ ============================================================================
+ Name        : Homework_4-Task_2
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Create an abstract Worker base class with the Print() method. Create four derived classes: President,
+Security, Manager, Engineer. Override the Print() method to print the information that corresponds to each employee type.
+ ============================================================================
+ */
+
+namespace Task_2
+{
+    abstract class Worker
+    {
+        string Name { get; set; }
+        string Surname { get; set; }
+        int Salary { get; set; }
+
+        protected Worker(string _Name, string _Surname, int _Salary)
+        {
+            Name = _Name;
+            Surname = _Surname;
+            Salary = _Salary;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Имя : " + Name);
+            Console.WriteLine("Фамилия : " + Surname);
+            Console.WriteLine("Зарплата : " + Salary);
+        }
+    }
+
+    class President : Worker
+    {
+        public President(string _Name, string _Surname, int _Salary) : base(_Name, _Surname, _Salary) { }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("Должность : Президент");
+        }
+    }
+
+    class Security : Worker
+    {
+        public Security(string _Name, string _Surname, int _Salary) : base(_Name, _Surname, _Salary) { }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("Должность : Охранник");
+        }
+    }
+
+    class Manager : Worker
+    {
+        public Manager(string _Name, string _Surname, int _Salary) : base(_Name, _Surname, _Salary) { }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("Должность : Менеджер");
+        }
+    }
+
+    class Engineer : Worker
+    {
+        public Engineer(string _Name, string _Surname, int _Salary) : base(_Name, _Surname, _Salary) { }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("Должность : Инженер");
+        }
+    }
+
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker[] workers = new Worker[4];
+
+            workers[0] = new President("Имя_1", "Фамилия_1", 10000);
+            workers[1] = new Security("Имя_2", "Фамилия_2", 3000);
+            workers[2] = new Manager("Имя_3", "Фамилия_3", 6000);
+            workers[3] = new Engineer("Имя_4", "Фамилия_4", 5000);
+
+            foreach (var worker in workers)
+            {
+                worker.Print();
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
