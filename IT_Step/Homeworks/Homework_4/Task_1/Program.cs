97dfd5f23415cc8242fb03d42dffee466402de57
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-01-22 21:12:05 +0200

    Revert "Updated Homework_4"
    This reverts commit c5e041fa504ca6975f9bccbf504623623119dc3b.

--- IT_Step/Homeworks/Homework_4/Task_1/Program.cs
+++ IT_Step/Homeworks/Homework_4/Task_1/Program.cs
@@ -17,18 +17,201 @@
 
 namespace Task_1
 {
+    class Money
+    {
+        private int _wholePart;
+        private int _fractionalPart;
+
+        public Money() : this(0, 0) { }
+
+        public Money(int wholePart, int fractionalPart)
+        {
+            if (wholePart < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(wholePart), wholePart, "Currency values cannot be negative!");
+            }
+
+            if (fractionalPart < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(fractionalPart), fractionalPart, "Currency values cannot be negative!");
+            }
+
+            _wholePart = wholePart;
+
+            if (fractionalPart <= 99)
+            {
+                _fractionalPart = fractionalPart;
+            }
+            else
+            {
+                // If the number of change coins is > 99, convert it to a whole part
+                normalizeParts();
+            }
+        }
+
+        public int WholePart
+        {
+            get => _wholePart;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                         nameof(value), value, "Currency values cannot be negative!");
+                }
+
+                _wholePart = value;
+            }
+        }
+
+        public int FractionalPart
+        {
+            get => _fractionalPart;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                         nameof(value), value, "Currency values cannot be negative!");
+                }
+
+                _fractionalPart = value;
+
+                // If the number of change coins became > 99, convert it to a whole part
+                if (_fractionalPart > 99)
+                {
+                    normalizeParts();
+                }
+            }
+        }
+
+        public void PrintAmount() => Console.WriteLine(_wholePart + "." + _fractionalPart);
+
+        private void normalizeParts()
+        {
+            _wholePart += _fractionalPart / 100;
+            _fractionalPart %= 100;
+        }
+    }
+
+    class Product : Money
+    {
+        private readonly string _productName;
+
+        public Product()
+           : base(0, 0)
+        {
+            _productName = "Default product name";
+        }
+
+        public Product(string name, int wholePart, int fractionalPart)
+            : base(wholePart, fractionalPart)
+        {
+            _productName = name;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        public void SetStartPrice()
+        {
+            int wholePart, fractionalPart;
+
+            Console.WriteLine("INITIAL PRODUCT PRICE :");
+
+            Console.WriteLine("Whole part :");
+
+            int.TryParse(Console.ReadLine(), out wholePart);
+
+            Console.WriteLine("Fractional part :");
+
+            int.TryParse(Console.ReadLine(), out fractionalPart);
+
+            SetPrice(wholePart, fractionalPart);
+        }
+
+        public void SetPrice(int wholePart, int fractionalPart)
+        {
+            base.WholePart = wholePart;
+            base.FractionalPart = fractionalPart;
+        }
+
+        public void IncreasePrice()
+        {
+            Console.WriteLine("На сколько увеличить цену ?");
+
+            Console.WriteLine("Целые единицы :");
+            int WholePart = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Разменные единицы :");
+            int FractionPart = Convert.ToInt32(Console.ReadLine());
+
+            IncreasePrice(WholePart, FractionPart);
+        }
+
+        public void IncreasePrice(int _iWholePart, int _iFractionPart)
+        {
+            base.WholePart += _iWholePart;
+            base.FractionalPart += _iFractionPart;
+        }
+
+        public void DecreasePrice()
+        {
+            Console.WriteLine("На сколько уменьшить цену ?");
+
+            Console.WriteLine("Целые единицы :");
+            int WholePart = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Разменные единицы :");
+            int FractionPart = Convert.ToInt32(Console.ReadLine());
+
+            DecreasePrice(WholePart, FractionPart);
+        }
+
+        public void DecreasePrice(int _iWholePart, int _iFractionPart)
+        {
+            base.WholePart -= _iWholePart;
+            base.FractionalPart -= _iFractionPart;
+        }
+
+        public string GetCurrentPrice()
+        {
+            return Convert.ToString(base.WholePart) + "." + Convert.ToString(base.FractionalPart);
+        }
+
+        public string GetName() => _productName;
+    }
+
     internal static class Program
     {
         static void Main(string[] args)
         {
-            Product product = new Product("Special #1", 10, 200);
-            Console.WriteLine(product.ToString());
+            Product myProduct_1 = new Product("Новый продукт", 10, 200);
+            Console.WriteLine("Текущая цена продукта " + myProduct_1.GetName() + " :" + myProduct_1.GetCurrentPrice());
 
-            product.IncreasePrice();
-            Console.WriteLine(product.ToString());
+            myProduct_1.SetStartPrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_1.GetCurrentPrice());
 
-            product.DecreasePrice();
-            Console.WriteLine(product.ToString());
+            myProduct_1.IncreasePrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_1.GetCurrentPrice());
+
+            myProduct_1.DecreasePrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_1.GetCurrentPrice());
+
+            Console.ReadLine();
+
+            Product myProduct_2 = new Product();
+            Console.WriteLine("Текущая цена продукта " + myProduct_2.GetName() + " :" + myProduct_2.GetCurrentPrice());
+
+            myProduct_2.SetStartPrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_2.GetCurrentPrice());
+
+            myProduct_2.IncreasePrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_2.GetCurrentPrice());
+
+            myProduct_2.DecreasePrice();
+            Console.WriteLine("Текущая цена продукта :" + myProduct_2.GetCurrentPrice());
 
             Console.ReadLine();
         }
--- IT_Step/Homeworks/Homework_4/Task_2/Program.cs
+++ IT_Step/Homeworks/Homework_4/Task_2/Program.cs
@@ -4,10 +4,8 @@
  Author      : Viacheslav Yefisko
  Version     : 0
  Copyright   : MIT License
- Description : Create an abstract base class Worker with the Print() method.
-               Then create four derived classes: President, Security, Manager,
-               Engineer. Override the Print() method to print the information
-               of each employee type.
+ Description : Create an abstract Worker base class with the Print() method. Create four derived classes: President,
+Security, Manager, Engineer. Override the Print() method to print the information that corresponds to each employee type.
  ============================================================================
  */
 
@@ -15,74 +13,62 @@ namespace Task_2
 {
     abstract class Worker
     {
-        private readonly string firstName;
-        private readonly string lastName;
-        private readonly int salary;
+        string Name { get; set; }
+        string Surname { get; set; }
+        int Salary { get; set; }
 
-        protected Worker(string firstName, string lastName, int salary)
+        protected Worker(string _Name, string _Surname, int _Salary)
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.salary = salary;
+            Name = _Name;
+            Surname = _Surname;
+            Salary = _Salary;
         }
 
         public virtual void Print()
         {
-            Console.WriteLine("First name : " + firstName);
-            Console.WriteLine("Last name  : " + lastName);
-            Console.WriteLine("Salary     : " + salary);
+            Console.WriteLine("Имя : " + Name);
+            Console.WriteLine("Фамилия : " + Surname);
+            Console.WriteLine("Зарплата : " + Salary);
         }
     }
 
     class President : Worker
     {
-        public President(string firstName, string lastName, int salary)
-            : base(firstName, lastName, salary)
-        { }
-
+        public President(string _Name, string _Surname, int _Salary) : base(_Name, _Surname, _Salary) { }
         public override void Print()
         {
             base.Print();
-            Console.WriteLine("Role       : President");
+            Console.WriteLine("Должность : Президент");
         }
     }
 
     class Security : Worker
     {
-        public Security(string firstName, string lastName, int salary)
-            : base(firstName, lastName, salary)
-        { }
-
+        public Security(string _Name, string _Surname, int _Salary) : base(_Name, _Surname, _Salary) { }
         public override void Print()
         {
             base.Print();
-            Console.WriteLine("Role       : Security");
+            Console.WriteLine("Должность : Охранник");
         }
     }
 
     class Manager : Worker
     {
-        public Manager(string firstName, string lastName, int salary)
-            : base(firstName, lastName, salary)
-        { }
-
+        public Manager(string _Name, string _Surname, int _Salary) : base(_Name, _Surname, _Salary) { }
         public override void Print()
         {
             base.Print();
-            Console.WriteLine("Role       : Manager");
+            Console.WriteLine("Должность : Менеджер");
         }
     }
 
     class Engineer : Worker
     {
-        public Engineer(string firstName, string lastName, int salary)
-            : base(firstName, lastName, salary)
-        { }
-
+        public Engineer(string _Name, string _Surname, int _Salary) : base(_Name, _Surname, _Salary) { }
         public override void Print()
         {
             base.Print();
-            Console.WriteLine("Role       : Engineer");
+            Console.WriteLine("Должность : Инженер");
         }
     }
 
@@ -90,12 +76,12 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Worker[] workers = [
-                new President("John", "Johnson", 10000),
-                new Security("Kevin", "Kevinson", 3000),
-                new Manager("Peter", "Peterson", 6000),
-                new Engineer("Donald", "Donaldson", 5000)
-                ];
+            Worker[] workers = new Worker[4];
+
+            workers[0] = new President("Имя_1", "Фамилия_1", 10000);
+            workers[1] = new Security("Имя_2", "Фамилия_2", 3000);
+            workers[2] = new Manager("Имя_3", "Фамилия_3", 6000);
+            workers[3] = new Engineer("Имя_4", "Фамилия_4", 5000);
 
             foreach (var worker in workers)
             {
