10a439985ffd1465c3d9b63d5c8889f59d5b7be5
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-02-01 21:04:47 +0200

    Updated Homework_11

---
+++ IT_Step/Homeworks/Homework_11/Task_1/Extension.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_1
+{
+    internal static class Extension
+    {
+        public static bool IsFibonacci(this int number) =>
+            Math.Sqrt(5 * Math.Pow(number, 2.0) + 4) % 1 == 0 ||
+            Math.Sqrt(5 * Math.Pow(number, 2.0) - 4) % 1 == 0;
+
+        // Wikipedia: A natural number N is a Fibonacci number if and only if
+        // at least one of (5*N)^2 + 4 or (5*N)^2 - 4 is a perfect square. And
+        // if the number is a square, then the root of this number will be an
+        // integer and the remainder from division it by 1 will be 0.
+    }
+}
---
+++ IT_Step/Homeworks/Homework_11/Task_1/FibonacciDriver.cs
@@ -0,0 +1,60 @@
+﻿namespace Task_1
+{
+    internal static class FibonacciDriver
+    {
+        public static void PrintFibonacciNumbersInNegativeRange()
+        {
+            int Current = 0;
+            int FirstPrevious = 1;
+            int SecondPrevious;
+
+            Console.WriteLine("Fibonacci numbers in negative range :");
+
+            for (int i = 0; i > -1000000; i--)
+            {
+                SecondPrevious = Current;
+                Current = FirstPrevious;
+                FirstPrevious += SecondPrevious;
+
+                if (Current.IsFibonacci())
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(Current);
+                    }
+                    else
+                    {
+                        Console.WriteLine(-Current);
+                    }
+                }
+            }
+        }
+
+        public static void PrintFibonacciNumbersInPositiveRange()
+        {
+            int Current = 0;
+            int FirstPrevious = 1;
+            int SecondPrevious;
+
+            Console.WriteLine("Числа Фибоначчи для положительного поддиапазона :");
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                SecondPrevious = Current;
+                Current = FirstPrevious;
+                FirstPrevious += SecondPrevious;
+
+                if (Current.IsFibonacci())
+                {
+                    Console.WriteLine(Current);
+                }
+            }
+        }
+        public static void RunTest()
+        {
+            PrintFibonacciNumbersInNegativeRange();
+            Console.WriteLine();
+            PrintFibonacciNumbersInPositiveRange();
+        }
+    }
+}
