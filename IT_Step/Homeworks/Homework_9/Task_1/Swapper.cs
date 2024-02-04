afccef8347d42d39e5c4e5d690047a40982795f0
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-02-04 19:20:52 +0200

    Updated Homework_9

---
+++ IT_Step/Homeworks/Homework_9/Task_1/Swapper.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_1
+{
+    internal static class Swapper<T>
+    {
+        public static void Swap(ref T val_1, ref T val_2)
+        {
+            T Temp = val_1;
+            val_1 = val_2;
+            val_2 = Temp;
+        }
+    }
+}
---
+++ IT_Step/Homeworks/Homework_9/Task_1/SwapperDriver.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_1
+{
+    internal static class SwapperDriver
+    {
+        public static void RunTest()
+        {
+            Console.Write("Enter first int number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second int number : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"First int number before swap : {a}");
+            Console.WriteLine($"Second int number before swap : {b}");
+
+            Swapper<int>.Swap(ref a, ref b);
+
+            Console.WriteLine();
+            Console.WriteLine($"First int number after swap : {a}");
+            Console.WriteLine($"Second int number after swap : {b}");
+        }
+    }
+}
