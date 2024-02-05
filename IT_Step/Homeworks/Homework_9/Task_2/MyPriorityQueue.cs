d191c52438b49c8e580286f181f2020fc4f04c70
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-02-05 18:00:58 +0200

    Updated Homework_9

---
+++ IT_Step/Homeworks/Homework_9/Task_2/MyPriorityQueue.cs
@@ -0,0 +1,84 @@
+﻿namespace Task_2
+{
+    internal class MyPriorityQueue<T>
+    {
+        // The list contains pairs "value - priority".
+        private readonly List<(T, uint)> ListContainer;
+
+        public MyPriorityQueue()
+        {
+            this.ListContainer = new List<(T, uint)>(0);
+        }
+
+        public int Size
+        {
+            get => this.ListContainer.Count;
+        }
+
+        // The Push method imlements logic "the lesser priority key is - the closer to
+        // the beginning its member is inserted".
+        public void Push(T value, uint priority)
+        {
+            if (this.IsEmpty())
+            {
+                ListContainer.Add((value, priority));
+            }
+            else
+            {
+                int Index = ListContainer.FindLastIndex(i => i.Item2 <= priority);
+
+                if ((Index == -1) && priority == 0)
+                {
+                    ListContainer.Insert(0, (value, priority));
+                }
+                else if (Index == -1)
+                {
+                    ListContainer.Add((value, priority));
+                }
+                else
+                {
+                    ListContainer.Insert(Index + 1, (value, priority));
+                }
+            }
+        }
+
+        public void Pop()
+        {
+            if (this.IsEmpty())
+            {
+                throw new InvalidOperationException(
+                    "Cannot pop the first element! The queue is empty!");
+            }
+
+            this.ListContainer.RemoveAt(0);
+        }
+
+        public T Peek()
+        {
+            if (this.IsEmpty())
+            {
+                throw new InvalidOperationException(
+                    "Cannot peek the first element! The queue is empty!");
+            }
+
+            return this.ListContainer[0].Item1;
+        }
+
+        public bool IsEmpty() => this.ListContainer.Count == 0;
+
+        public void Clear()
+        {
+            this.ListContainer.Clear();
+        }
+
+        public void Print()
+        {
+            foreach (var item in this.ListContainer)
+            {
+                Console.Write(item.Item1 + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ IT_Step/Homeworks/Homework_9/Task_2/MyPriorityQueueDriver.cs
@@ -0,0 +1,45 @@
+﻿namespace Task_2
+{
+    internal static class MyPriorityQueueDriver
+    {
+        public static void RunTest()
+        {
+            try
+            {
+                var priorityQueue = new MyPriorityQueue<int>();
+
+                priorityQueue.Push(1, 1);
+                priorityQueue.Push(2, 1);
+                priorityQueue.Push(2, 1);
+                priorityQueue.Push(3, 2);
+                priorityQueue.Push(3, 2);
+
+                priorityQueue.Print();
+
+                Console.WriteLine("Size of the queue : " + priorityQueue.Size);
+
+                priorityQueue.Push(15, 1);
+                priorityQueue.Print();
+
+                priorityQueue.Push(10, 0);
+                priorityQueue.Print();
+
+                priorityQueue.Push(11, 0);
+                priorityQueue.Print();
+
+                Console.WriteLine("Size of the queue : " + priorityQueue.Size);
+
+                priorityQueue.Clear();
+                priorityQueue.Print();
+
+                Console.WriteLine("Size of the queue : " + priorityQueue.Size);
+
+                priorityQueue.Pop();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
