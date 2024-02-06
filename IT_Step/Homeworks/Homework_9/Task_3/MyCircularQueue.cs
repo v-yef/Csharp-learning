f4b47ec58377bed221475c8df9c98e90a11fef91
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-02-06 19:55:19 +0200

    Updated Homework_9

---
+++ IT_Step/Homeworks/Homework_9/Task_3/MyCircularQueue.cs
@@ -0,0 +1,71 @@
+﻿namespace Task_3
+{
+    internal class MyCircularQueue<T>
+    {
+        private readonly List<T> ListContainer;
+
+        public MyCircularQueue()
+        {
+            this.ListContainer = new List<T>(0);
+        }
+
+        public int Size
+        {
+            get => this.ListContainer.Count;
+        }
+
+        public void Push(T _Value) => this.ListContainer.Add(_Value);
+
+        public void Pop()
+        {
+            if (this.Empty())
+            {
+                throw new InvalidOperationException(
+                    "Cannot pop the first element! The queue is empty!");
+            }
+
+            this.ListContainer.RemoveAt(0);
+        }
+
+        public T Peek()
+        {
+            if (this.Empty())
+            {
+                throw new InvalidOperationException(
+                    "Cannot peek the first element! The queue is empty!");
+            }
+
+            return this.ListContainer[0];
+        }
+
+        public bool Empty() => this.ListContainer.Count == 0;
+
+        public void Clear() => this.ListContainer.Clear();
+
+        // "Circling" the queue. The first element is moved to the end.
+        public void Circle()
+        {
+            if (this.ListContainer.Count < 2)
+            {
+                throw new InvalidOperationException(
+                    "The queue has too few elements to be circular!");
+            }
+
+            T Val = this.Peek();
+
+            this.Pop();
+
+            this.Push(Val);
+        }
+
+        public void Print()
+        {
+            foreach (var item in this.ListContainer)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ IT_Step/Homeworks/Homework_9/Task_3/MyCircularQueueDriver.cs
@@ -0,0 +1,46 @@
+﻿namespace Task_3
+{
+    internal static class MyCircularQueueDriver
+    {
+        public static void RunTest()
+        {
+            try
+            {
+                var circularQueue = new MyCircularQueue<int>();
+
+                circularQueue.Push(1);
+                circularQueue.Push(2);
+                circularQueue.Push(3);
+                circularQueue.Push(4);
+                circularQueue.Push(5);
+                circularQueue.Print();
+                Console.WriteLine("Size of the queue : " + circularQueue.Size);
+
+                Console.WriteLine("Popping two elements from the queue...");
+                circularQueue.Pop();
+                circularQueue.Pop();
+                circularQueue.Print();
+                Console.WriteLine("Size of the queue : " + circularQueue.Size);
+
+                Console.WriteLine("Scrolling the queue one time...");
+                circularQueue.Circle();
+                circularQueue.Print();
+
+                Console.WriteLine("Scrolling the queue one more time...");
+                circularQueue.Circle();
+                circularQueue.Print();
+
+                Console.WriteLine("The queue is cleared now! ");
+                circularQueue.Clear();
+                circularQueue.Print();
+                Console.WriteLine("Size of the queue : " + circularQueue.Size);
+
+                circularQueue.Pop();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
