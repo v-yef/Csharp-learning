f70bffd0254d96a34f8b9113cc416603b23b75bc
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-02-14 18:50:25 +0200

    Added Homework_15

---
+++ IT_Step/Homeworks/Homework_15/Task_1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Task_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if ((e.Button == MouseButtons.Left) && Control.ModifierKeys == Keys.Control)
+            {
+                Environment.Exit(0);
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                this.Text = $"Window width = {this.Width}, Window height = {this.Height}";
+                Thread.Sleep(500);
+            }
+
+            if (e.Button == MouseButtons.Left)
+            {
+
+                if (e.X < 10 ||
+                    e.X > this.ClientSize.Width - 10 ||
+                    e.Y < 10 ||
+                    e.Y > this.ClientSize.Height - 10)
+                {
+                    MessageBox.Show("This point is outside the rectangle.",
+                                    "Selected position", MessageBoxButtons.OK);
+                }
+                else if ((e.X == 10 || e.X == this.ClientSize.Width - 10) ||
+                        (e.Y == 10 || e.Y == this.ClientSize.Height - 10))
+                {
+                    MessageBox.Show("This point is on the border of the rectangle.",
+                                    "Selected position", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("This point is inside the rectangle.",
+                                    "Selected position", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = $"X = {e.X}, Y = {e.Y}";
+        }
+    }
+}
---
+++ IT_Step/Homeworks/Homework_15/Task_1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ ============================================================================
+ Name        : Homework_15-Task_1
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Imagine that you have a rectangle on your form, the borders of
+               which are 10 pixels away from the borders of the form. You must
+               create handlers for:
+               - left mouse button click, that displays a message about where
+               the current point is: inside the rectangle, outside, on the
+               border of the rectangle. If the left mouse button was pressed
+               with a Control (Ctrl) key, the application should close.
+               - right mouse button click, which displays information about the
+               size of the working area in the window title: Width = x,
+               Height = y, where x and y are the corresponding parameters of
+               the window.
+               - moving a mouse pointer within the working area, which should
+               output current mouse coordinates x and y to the window title.
+ ============================================================================
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace Task_1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
