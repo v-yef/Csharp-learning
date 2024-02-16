26908bf7d45071221611e2179cfcc1c7dbdb7d4a
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-02-16 19:42:38 +0200

    Updated Homework_40

---
+++ IT_Step/Homeworks/Homework_40/Task_2/Menu.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal static class Menu
+    {
+        public static int VerticalMenu(string[] elements, ConsoleColor foregroundColor, ConsoleColor backgroundColor,
+                                        int upLeft_x, int upLeft_y)
+        {
+            int maxLen = 0;
+
+            foreach (var item in elements)
+            {
+                if (item.Length > maxLen)
+                {
+                    maxLen = item.Length;
+                }
+            }
+
+            ConsoleColor initialForegroundColor = Console.ForegroundColor;
+            ConsoleColor initialBackgroundColor = Console.BackgroundColor;
+
+            Console.BackgroundColor = foregroundColor;
+            Console.ForegroundColor = backgroundColor;
+
+            Console.SetCursorPosition(0, 0);
+
+            int x = Console.CursorLeft;
+            int y = Console.CursorTop;
+
+            int pos = 0;
+
+            while (true)
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+
+                    Console.CursorVisible = false;
+                    Console.SetCursorPosition(x + upLeft_x, y + i + upLeft_y);
+
+                    if (i == pos)
+                    {
+                        Console.BackgroundColor = foregroundColor;
+                        Console.ForegroundColor = backgroundColor;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = backgroundColor;
+                        Console.ForegroundColor = foregroundColor;
+                    }
+
+                    Console.Write(elements[i].PadCenter(maxLen));
+                }
+
+                Console.ForegroundColor = initialForegroundColor;
+                Console.BackgroundColor = initialBackgroundColor;
+
+                ConsoleKey consoleKey = Console.ReadKey().Key;
+
+                switch (consoleKey)
+                {
+
+                    case ConsoleKey.Enter:
+                        return pos;
+
+                    case ConsoleKey.Escape:
+                        return -1;
+
+                    case ConsoleKey.UpArrow:
+                        if (pos > 0)
+                        {
+                            pos--;
+                        }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (pos < elements.Length - 1)
+                        {
+                            pos++;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    static class Extensions
+    {
+        public static string PadCenter(this string sourceString, int length)
+        {
+
+            if ((sourceString.Length >= length) || (sourceString.Length + 1 == length))
+            {
+                return sourceString;
+            }
+
+            int charNumberToAdd = (length - sourceString.Length) / 2;
+
+            string result = "";
+
+            for (int i = 0; i < charNumberToAdd; i++)
+            {
+                result += " ";
+            }
+
+            result += sourceString;
+
+            for (int i = 0; i < charNumberToAdd; i++)
+            {
+                result += " ";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ IT_Step/Homeworks/Homework_40/Task_2/Program.cs
@@ -0,0 +1,127 @@
+﻿/*
+ ============================================================================
+ Name        : Homework_40-Task_2
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Write a program, that uses legacy code. You need to use the
+               FindWindow and SendMessage functions from the Windows API. The
+               application must search for the window of your application (it
+               can be implemented using Windows Forms, etc.). If the window is
+               found, the following message must be sent to it depending on the
+               user's choice:
+                - change the window title to the one entered by user,
+                - close the window.
+ ============================================================================
+ */
+
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Task_2
+{
+    internal static class ExternalFunctions
+    {
+        public const uint WM_SETTEXT = 0x000C;
+        public const uint WM_CLOSE = 0x0010;
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        public static extern IntPtr FindWindowByCaption(
+            IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(
+            IntPtr hWnd, uint Msg, IntPtr wParam, StringBuilder lParam);
+    }
+
+    internal class WindowProcessor
+    {
+        public void DoWork()
+        {
+            IntPtr windowHandler = this.GetWindowHandler("Test Window");
+
+            if (windowHandler != IntPtr.Zero)
+            {
+                Console.WriteLine("Window was found. Choose action:");
+
+                int choice = Menu.VerticalMenu(new string[]{"Change window header",
+                                                "Close window",
+                                                "Exit program" },
+                                                ConsoleColor.White, ConsoleColor.Black, 0, 2);
+                Console.Clear();
+
+                switch (choice)
+                {
+                    case 0:
+                        sendMessageChangeWindowHeader(windowHandler);
+                        break;
+                    case 1:
+                        sendMessageWindowClose(windowHandler);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Window was not found. Press any key to exit...");
+                Console.Read();
+            }
+        }
+
+        private IntPtr GetWindowHandler(string windowName) =>
+            ExternalFunctions.FindWindowByCaption(IntPtr.Zero, windowName);
+
+        private void sendMessageChangeWindowHeader(IntPtr windowHandler)
+        {
+            Console.Write("Enter header: ");
+            string header = Console.ReadLine();
+
+            StringBuilder stringBuilder = new StringBuilder(header);
+
+            IntPtr result = ExternalFunctions.SendMessage(
+                windowHandler, ExternalFunctions.WM_SETTEXT, IntPtr.Zero, stringBuilder);
+
+            if (result != IntPtr.Zero)
+            {
+                Console.WriteLine("The window header was changed. Check the window.");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong. The header was not changed.");
+            }
+        }
+
+        private void sendMessageWindowClose(IntPtr windowHandler)
+        {
+            IntPtr result = ExternalFunctions.SendMessage(
+                windowHandler, ExternalFunctions.WM_CLOSE, IntPtr.Zero, null);
+
+            if (result == IntPtr.Zero)
+            {
+                Console.WriteLine("The window was closed. Check the window.");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong. The window was not closed.");
+            }
+        }
+    }
+
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var winForm = new WinForm();
+            winForm.Show();
+
+            var windowProcessor = new WindowProcessor();
+            windowProcessor.DoWork();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+    }
+}
