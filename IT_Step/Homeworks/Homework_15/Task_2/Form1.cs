f0dd7495899a82d9339299ebcf5f58d634c4af50
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-02-17 18:59:50 +0200

    Updated Homework_15

---
+++ IT_Step/Homeworks/Homework_15/Task_2/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    public partial class Form1 : Form
+    {
+        private int startX;
+        private int startY;
+        private int number = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.startX = e.X;
+                this.startY = e.Y;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                var label = new Label
+                {
+                    BorderStyle = BorderStyle.FixedSingle
+                };
+
+                // Define the element's anchor point (upper left corner),
+                // based on its start and final coordinates.
+                if (e.X > startX && e.Y < startY)
+                {
+                    label.Location = new Point(startX, e.Y);
+                }
+                else if (e.X > startX && e.Y > startY)
+                {
+                    label.Location = new Point(startX, startY);
+                }
+                else if (e.X < startX && e.Y > startY)
+                {
+                    label.Location = new Point(e.X, startY);
+                }
+                else
+                {
+                    label.Location = new Point(e.X, e.Y);
+                }
+
+                // If the element meets the conditions, create it.
+                if (Math.Abs(e.X - startX) > 10 && Math.Abs(e.Y - startY) > 10)
+                {
+                    label.Size = new Size(Math.Abs(e.X - startX), Math.Abs(e.Y - startY));
+                    label.Text = $"{number}";
+                    label.ForeColor = Color.Black;
+                    label.BackColor = Color.DarkGray;
+
+                    // Add the element to the form controls collection.
+                    this.Controls.Add(label);
+
+                    // Subscribe to click events on the element.
+                    label.MouseClick += LabelMouseClickEvent;
+                    label.MouseDoubleClick += LabelMouseDoubleClickEvent;
+
+                    this.number++;
+                }
+                else
+                {
+                    MessageBox.Show("The element must be greater than 10х10", "Error",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void LabelMouseClickEvent(object sender, MouseEventArgs e)
+        {
+            int labelNumber = 0;
+
+            if (e.Button == MouseButtons.Right)
+            {
+                foreach (Label item in Controls)
+                {
+                    // Get the coordinates of the click.
+                    Point location = item.PointToScreen(Point.Empty);
+
+                    // Define the element(s) on which the click is made.
+                    if (MousePosition.X > location.X &&
+                        MousePosition.X < location.X + item.Width &&
+                        MousePosition.Y > location.Y &&
+                        MousePosition.Y < location.Y + item.Height)
+                    {
+                        // Select the element with the highest serial number.
+                        if (labelNumber < Convert.ToInt32(item.Text))
+                        {
+                            labelNumber = Convert.ToInt32(item.Text);
+                        }
+
+                        this.Text = $"Элемент {item.Text}, " +
+                                    $"S = {item.Width * item.Height}, " +
+                                    $"Х = {item.Location.X}, " +
+                                    $"Y = {item.Location.Y}";
+                    }
+                }
+            }
+        }
+
+        private void LabelMouseDoubleClickEvent(object sender, MouseEventArgs e)
+        {
+            int labelNumber = this.number;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                foreach (Label item in Controls)
+                {
+                    // Get the coordinates of the click.
+                    Point location = item.PointToScreen(Point.Empty);
+
+                    // Define the element(s) on which the click is made.
+                    if (MousePosition.X > location.X &&
+                        MousePosition.X < location.X + item.Width &&
+                        MousePosition.Y > location.Y &&
+                        MousePosition.Y < location.Y + item.Height)
+                    {
+                        // Select the element with the highest serial number.
+                        if (labelNumber > Convert.ToInt32(item.Text))
+                        {
+                            labelNumber = Convert.ToInt32(item.Text);
+                        }
+                    }
+                }
+
+                foreach (Label item in Controls)
+                {
+                    // Find the element in a form controls collection
+                    if (labelNumber == Convert.ToInt32(item.Text))
+                    {
+                        // and delete it.
+                        Controls.Remove(item);
+                        item.MouseClick -= LabelMouseClickEvent;
+                        item.MouseDoubleClick -= LabelMouseDoubleClickEvent;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IT_Step/Homeworks/Homework_15/Task_2/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ ============================================================================
+ Name        : Homework_15-Task_2
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Create a winform application with following functionality:
+                - The user clicks on the form with the left mouse button and
+                moves the mouse over the form holding the button. When the
+                button is released a rectangle must be drawn, according to the
+                start and final coordinates of such movement. The rectangle must
+                contain a serial number (the order of its appearance on the form).
+                - The minimal rectangle size is 10×10. If user tries to create
+                an element of smaller size, a corresponding warning is shown.
+                - When user right-clicks over the rectangle surface, the program
+                displays the information about the rectangle (its area and
+                coordinates) in the window title. If there are several rectangles,
+                then the preference is given to the one with the highest serial
+                number.
+                - When user double-clicks the left mouse button over the rectangle
+                surface, it must disappear from the form. If there are several
+                rectangles at the click point, then the preference is given to
+                the one with the lowest serial number.
+ ============================================================================
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
