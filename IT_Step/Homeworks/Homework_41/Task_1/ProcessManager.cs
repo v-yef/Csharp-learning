d08821b8ac6cf6b57ef5b4b80670271c244a1191
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-02-18 20:06:49 +0200

    Added Homework_41

---
+++ IT_Step/Homeworks/Homework_41/Task_1/ProcessManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace Task_1
+{
+    internal static class ProcessManager
+    {
+        public static void Work()
+        {
+            Console.WriteLine("Press any key to start process...");
+            Console.ReadKey();
+
+            try
+            {
+                var process = new Process();
+
+                if (process is null)
+                {
+                    Console.WriteLine("Cannot create new process!");
+                    return;
+                }
+
+                process.StartInfo.FileName = "notepad.exe";
+
+                if (process.Start())
+                {
+                    Console.WriteLine("Process started successfully!");
+                    process.WaitForExit();
+                    Console.WriteLine($"Process was closed. Exit code: {process.ExitCode}");
+                }
+                else
+                {
+                    Console.WriteLine("Process cannot start!");
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine($"{exc.Message}");
+            }
+        }
+    }
+}
---
+++ IT_Step/Homeworks/Homework_41/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ ============================================================================
+ Name        : Homework_41-Task_1
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Create an application that starts a child process and waits
+               untill it is complete. When the child process is complete, the
+               parent application must display the exit code.
+ ============================================================================
+ */
+
+namespace Task_1
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            ProcessManager.Work();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
