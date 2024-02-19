a7b6f189966fadb1a16f6c2255e3c71287099dc7
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2024-02-19 20:43:26 +0200

    Updated Homework_41

--- IT_Step/Homeworks/Homework_41/Task_2/ProcessManager.cs
+++ IT_Step/Homeworks/Homework_41/Task_2/ProcessManager.cs
@@ -1,10 +1,10 @@
 ﻿using System.Diagnostics;
 
-namespace Task_1
+namespace Task_2
 {
-    internal static class ProcessManager
+    internal class ProcessManager
     {
-        public static void Work()
+        public void Work()
         {
             Console.WriteLine("Press any key to start process...");
             Console.ReadKey();
@@ -25,7 +25,7 @@ namespace Task_1
                 {
                     Console.WriteLine("Process started successfully!");
 
-                    process.WaitForExit();
+                    this.maintainProcess(process);
 
                     Console.WriteLine($"Process was closed. Exit code: {process.ExitCode}");
                 }
@@ -38,6 +38,30 @@ namespace Task_1
             {
                 Console.WriteLine($"\n\n{exc.Message}");
             }
+        }
+
+        private void maintainProcess(Process process)
+        {
+            Console.WriteLine("Choose action:");
+
+            int choice = Menu.VerticalMenu(["Wait for process exit",
+                                            "Kill process"],
+                                            ConsoleColor.White, ConsoleColor.Black, 0, 4);
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("\n\nWaiting user to close the process...");
+                    process.WaitForExit();
+                    break;
+                case 1:
+                    Console.WriteLine("\n\nClosing process right now...");
+                    process.Kill();
+                    break;
+                default:
+                    break;
+            }
+
+            return;
         }
     }
 }
---
+++ IT_Step/Homeworks/Homework_41/Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ ============================================================================
+ Name        : Homework_41-Task_2
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Create an application that starts a child process. Depending on 
+               user's choice the application must wait untill the child process
+               is complete or terminate the child process. The exit code must
+               be displayed afterwards.
+ ============================================================================
+ */
+
+namespace Task_2
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            var processManager = new ProcessManager();
+            processManager.Work();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
